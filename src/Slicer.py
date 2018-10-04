923c28daade7431c02038959a249da991f6aa7a1
Marc Etter <marc.etter@swissonline.ch>
2018-10-04 21:28:57 +0200

    slice dummymap into equal square parts

--- src/Slicer.py
+++ src/Slicer.py
@@ -1,61 +1,20 @@
-from keras.models import Sequential
-from keras.layers import Dense
-import numpy
-# fix random seed for reproducibility
-numpy.random.seed(10)
+import os
+from PIL import Image
 
-import pandas as pd
+file = "./DummyMap.png"
+map = Image.open(file)
 
-data_set_url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv'
-HEADER = [
-    # from https://github.com/jbrownlee/Datasets/blob/master/pima-indians-diabetes.names
-    'Number of times pregnant',
-    'Glucosse level',
-    'blood pressure',
-    'skin fold thickness',
-    'insulin',
-    'BMI',
-    'Diabetes pedigree function',
-    'Age',
-    'Class variable',
-]
-df = pd.read_csv(data_set_url, names=HEADER)
-df.tail()
+boxSize = 100
 
-# result to be explained ("Output variable")
-out_variable = df['Class variable']
+xsize, ysize = map.size
 
-# explaining variables ("Input variable")
-input_matrix = df.loc[:, 'Number of times pregnant':'Age']
-# .values = numpy array
+parts = []
+for x in range(0, xsize, boxSize):
+    for y in range(0, ysize, boxSize):
+        parts.append(map.crop((x, y, x + boxSize, y + boxSize)))
 
-input_matrix.head()
+if not os.path.exists("./out"):
+    os.makedirs("./out")
 
-out_variable.head()
-
-# normalize explaining variables
-
-input_matrix_normalized = input_matrix.div(df.sum(axis=1), axis=0)
-input_matrix_normalized.head()
-
-# use numpy array for statistically use variables X (input) and Y (output)
-X = input_matrix_normalized.values
-Y = out_variable.values
-
-# create model
-model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-# Compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# Fit the model
-# model.fit(X, Y, epochs=150, batch_size=1000, callbacks=[tensor_board_callback])
-# use verbose=0 to suppress output
-# model.fit(X, Y, epochs=150, batch_size=1000, callbacks=[tensor_board_callback], verbose=0)
-model.fit(X, Y, epochs=10, batch_size=100)
-
-# evaluate the model
-scores = model.evaluate(X, Y)
-print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+for i, part in enumerate(parts):
+    part.save("./out/part" + str(i) + ".png", "PNG")
