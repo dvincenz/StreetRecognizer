6c8d2d74ef8e13eac78fa63fbab720f0fdb091ca
Dumeni Vincenz <dvincenz@outlook.com>
2018-10-24 18:03:40 +0200

    add download script

---
+++ src/utils/downloadOrthofotos.ps1
@@ -0,0 +1,10 @@
+$baseUrl = 'https://maps.zh.ch/download/orthofoto/sommer/2014/rgb/jpeg/'
+
+$allUrls = (Invoke-WebRequest –Uri $baseUrl).Links
+
+
+foreach ($url in $allUrls){
+    $outFile = $url.outerText
+    Invoke-WebRequest -Uri "$($baseUrl)$($outFile)" -OutFile $outFile
+}
+    
