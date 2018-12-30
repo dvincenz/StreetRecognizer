f5f78d5a91579017ebd000206471e1bae5dc0c4a
Dumeni Vincenz <dvincenz@outlook.com>
2018-12-30 07:53:25 +0100

    add optionaly use of azure blob storage

--- src/ImageProvider/ImageProviderConfig.py
+++ src/ImageProvider/ImageProviderConfig.py
@@ -1,9 +1,17 @@
 class ImageProviderConfig:
-    def __init__(self, azure_blob_key: str = "key not set", azure_blob_account: str = "swisstopo", azure_blob_name: str = "rawdata", image_url: str = "../data/in/ortho"):
+    def __init__(self, 
+            azure_blob_key: str = "key not set", 
+            azure_blob_account: str = "swisstopo", 
+            azure_blob_name: str = "rawdata", 
+            input_path: str = "../data/in/ortho/raw",
+            output_path: str = "../data/in/ortho/wgs84",
+            is_azure: bool = False):
         self.azure_blob_key = azure_blob_key
         self.azure_blob_account = azure_blob_account
-        self.image_url = image_url
+        self.input_url = input_path
+        self.output_url = output_path
         self.azure_blob_name = azure_blob_name
+        self.is_azure = is_azure
 
     def set_azure_key_from_config(self, config_file: str):
         self.azure_blob_key = open(config_file, "r").read()
