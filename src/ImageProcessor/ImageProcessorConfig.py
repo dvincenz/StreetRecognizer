dceac175a327e0ed7d0efc095df1b37cd42cb5ff
Dumeni Vincenz <dvincenz@outlook.com>
2019-01-03 19:14:17 +0100

    change filter attribute

--- src/ImageProcessor/ImageProcessorConfig.py
+++ src/ImageProcessor/ImageProcessorConfig.py
@@ -12,16 +12,37 @@ class ImageProcessorConfig:
             self.filter = self._get_filter()
         else:
             self.filter = filter
+        if not self.filter.values.get("unclassified"):
+            self.filter.values["unclassified"] = 1
         if not self.filter.values.get("default"):
             self.filter.values["default"] = 1
-        if not self.filter.values.get("empty"):
-             self.filter.values["empty"] = 255
+
 
     @staticmethod
     def _get_filter():
-        filter_values = {}
-        filter_values["asphalt"] = 10
-        filter_values["gravel"] = 20
-        filter_values["ground"] = 30
-        return ImageProcessorFilter("burn", "surface", filter_values)
+        filter = {}
+        filter["primary"] = 180
+        filter["primary_link"] = 180
+        filter["motorway"] = 170
+        filter["motorway_link"] = 170
+        filter["trunk"] = 160
+        filter["trunk_link"] = 160
+        filter["secondary"] = 150
+        filter["secondary_link"] = 150
+        filter["tertiary"] = 140
+        filter["tertiary_link"] = 140
+        filter["residential"] = 130
+        filter["living_street"] = 120
+        filter["track"] = 110
+        filter["construction"] = 100
+        filter["service"] = 90
+        filter["cycleway"] = 80
+        filter["footway"] = 70
+        filter["steps"] = 60
+        filter["footway"] = 50
+        filter["path"] = 40
+        filter["pedestrian"] = 30
+        filter["footway"] = 20
+        filter["path"] = 10
+        return ImageProcessorFilter("burn", "highway", filter)
 
