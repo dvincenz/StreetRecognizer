dae7c622e454abf83e12c7d0870a770cebdecaec
Marc Etter <marc.etter@swissonline.ch>
2019-01-12 18:25:17 +0100

    Added dump option to metaextractor

--- src/metaextractor/__init__.py
+++ src/metaextractor/__init__.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import pandas
 import sqlite3
 
 from metaextractor.OrthoMetaExtractor import OrthoMetaExtractor
@@ -9,10 +10,11 @@ from metaextractor.TileMetaExtractor import TileMetaExtractor
 
 def _parse_args():
     parser = argparse.ArgumentParser(description='Extract relevant metadata from Ortho-Photos and -Tiles and store them in a database for easier access.')
+    parser.add_argument('datasource', metavar='DB', type=str, help='path to the database file')
     parser.add_argument('-o', '--ortho-data', type=str, help='path to the ortho photos to index')
     parser.add_argument('-t', '--tile-data', type=str, help='path to the JSON containing the tile data for an ortho photo')
-    parser.add_argument('datasource', metavar='DB', type=str, help='path to the database file')
     parser.add_argument('--clean', action='store_true', help='Cleans (drops) the database and re-initializes it')
+    parser.add_argument('--dump', metavar='TABLE', help='Dumps the content of the given table, instead of extracting and inserting any data')
     return vars(parser.parse_args())
 
 
@@ -22,7 +24,8 @@ def run():
         ortho_data_path=args['ortho_data'],
         tile_data_path=args['tile_data'],
         data_source_path=args['datasource'],
-        clean=args['clean']
+        clean=args['clean'],
+        dump=args['dump']
     )
 
 
@@ -35,7 +38,7 @@ def _init_db(cursor: sqlite3.Cursor):
     print('Database initialized!')
 
 
-def extractor(ortho_data_path: str, tile_data_path: str, data_source_path: str, clean: bool = False):
+def extractor(ortho_data_path: str, tile_data_path: str, data_source_path: str, clean: bool = False, dump: str = None):
     if os.path.exists(data_source_path):
         if clean:
             os.remove(data_source_path)
@@ -51,6 +54,11 @@ def extractor(ortho_data_path: str, tile_data_path: str, data_source_path: str, 
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
     if cursor.fetchone() is None:
         _init_db(cursor)
+
+    if not dump is None:
+        # Yes, yes, SQL Injection, but table-name is not parameterizable in sqlite3
+        print(pandas.read_sql_query("SELECT * FROM " + dump, conn))
+        return 0
 
     if ortho_data_path:
         print('Extracting ortho metadata...')
